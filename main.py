75b3c2700de5c25c1e27978461f80f5dd10cbdeb
Ervinas Miltenis <ervinasmiltenis@gmail.com>
2023-05-25 19:56:09 +0100

    Added respective host on which the account credentials belong to

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from secret import passwords
 
 username = 'sunflower'
 
-print(f"Steel Mountain 9000x Password Database")
+print(f"--=Steel Mountain 9000x Password Database=--")
 for entry in passwords:
-    for username, password in entry.items():
-        print(f"{username}:",end="")
-        for asterisk in range(len(password)):
-            print(f"*", end="")
-        print("")
+    for account, credentials in entry.items():
+        for username, password in credentials.items():
+            print(f"{account} - {username}:", end="")
+            for asterisk in range(len(password)):
+                print(f"*", end="")
+            print("")
