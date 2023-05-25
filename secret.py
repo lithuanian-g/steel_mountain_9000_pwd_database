5e0eeaa4e788fba6167daeefd3cf16f24ec308de
Ervinas Miltenis <ervinasmiltenis@gmail.com>
2023-05-25 19:07:23 +0100

    Changed from array of passwords to dictionary values with equivalent usernames

--- secret.py
+++ secret.py
@@ -1,2 +1,6 @@
-# This is some Python code
-password = "secret123"
+# Password Database
+mssql_password = {"sa":"sa"}
+domain_admin_password = {"d.smith":"s0-s0-j3410u5!-p3ngu1n"}
+web01_password = {"admin":"Winter2023"}
+
+passwords = [mssql_password, domain_admin_password, web01_password]
