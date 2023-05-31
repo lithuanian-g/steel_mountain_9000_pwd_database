d45b9b5cdea5bb0df932b3660bdafb1ead5f2e39
lituanica <135149686+lituanica@users.noreply.github.com>
2023-05-31 17:11:14 +0100

    Improved Header Text

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from secret import passwords
 
 username = 'sunflower'
 
+print("-" * 44)
 print(f"--=Steel Mountain 9000x Password Database=--")
+print("-" * 44)
 for entry in passwords:
     for account, credentials in entry.items():
         for username, password in credentials.items():
